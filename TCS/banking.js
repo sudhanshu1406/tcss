b370881f81a1b0bdbe5a9db6cefad2968884319b
sudhanshu1406 <sudhanshumehto693@gmail.com>
2024-11-11 11:46:12 +0530

    add

---
+++ TCS/banking.js
@@ -0,0 +1,47 @@
+function zoomin1(){
+    let selecteding = document.querySelector('#img1');
+    selecteding.style.scale = "1.2"
+    selecteding.style.transition = "scale 1.5s";
+    console.log(selecteding);
+}
+function initial1(){
+    let selecteding = document.querySelector('#img1')
+    selecteding.style = "1";
+    selecteding.style.transition = "scale 1.5s"
+}
+
+function zoomin2(){
+    let selecteding = document.querySelector('#img2');
+    selecteding.style.scale = "1.2"
+    selecteding.style.transition = "scale 1.5s";
+    console.log(selecteding);
+}
+function initial2(){
+    let selecteding = document.querySelector('#img2')
+    selecteding.style = "1";
+    selecteding.style.transition = "scale 1.5s"
+}
+
+function zoomin3(){
+    let selecteding = document.querySelector('#img3');
+    selecteding.style.scale = "1.2"
+    selecteding.style.transition = "scale 1.5s";
+    console.log(selecteding);
+}
+function initial3(){
+    let selecteding = document.querySelector('#img3')
+    selecteding.style = "1";
+    selecteding.style.transition = "scale 1.5s"
+}
+
+function zoomin4(){
+    let selecteding = document.querySelector('#img4');
+    selecteding.style.scale = "1.2"
+    selecteding.style.transition = "scale 1.5s";
+    console.log(selecteding);
+}
+function initial4(){
+    let selecteding = document.querySelector('#img4')
+    selecteding.style = "1";
+    selecteding.style.transition = "scale 1.5s"
+}
